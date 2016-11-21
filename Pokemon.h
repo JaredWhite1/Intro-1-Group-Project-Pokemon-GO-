87cf10055d172d4381a57b1844c984bf049e95a4
JaredWhite1 <Jared_White1@baylor.edu>
2016-11-20 18:17:17 -0600

    some random things

--- Pokemon.h
+++ Pokemon.h
@@ -1,78 +1,97 @@
 #ifndef POKEMON_H_INCLUDED
 #define POKEMON_H_INCLUDED
-
-#include <ostream>
+#include "SDL_Plotter.h"
+#include "background.h"
 #include <fstream>
-#include "SDL_plotter.h"
 
-enum DIR {UP, DOWN, LEFT, RIGHT};
+enum DIR{UP,DOWN,LEFT,RIGHT};
 
-struct Point
-{
-    int x,y;
-    Point(int x=0,int y=0);
-};
 
-class Pokemon
-{
+
+class Pokemon{
     private:
-        Point loc;
         Point oldLoc;
         int speed;
-        int R,G,B;
-
+        int R, G, B;
     public:
+        bool deleted = false;
+        Color pic[80][80];
+        int dimension1, dimension2;
+        Point loc;
+        vector <vector<Point> > background;
+        string pokemonName;
         Pokemon();
-        void draw(SDL_Plotter&,string);
-        void erase(SDL_Plotter&,int,int);
+        ~Pokemon();
+        void createSprite(string pokemonName);
+        void draw(SDL_Plotter&);
+        void erase(SDL_Plotter&);
         void move(DIR);
         void setLoc(Point);
+        void openFile(string pokemonName);
+        void scopeBackground(vector <vector<Point> > scopeBackground);
+        void deletePokemon(SDL_Plotter&, bool& deleted);
 };
 
-Point::Point(int a, int b)
+Pokemon:: Pokemon()
 {
-    x = a;
-    y = b;
-}
-
-Pokemon::Pokemon()
-{
-    oldLoc = loc;
+    oldLoc.x = loc.x;
+    oldLoc.y = loc.y;
     speed = 5;
 }
 
-void Pokemon::draw(SDL_Plotter& g,string fileName)
+Pokemon:: ~Pokemon()
 {
-    ifstream myFile;
-    int dimension1,dimension2;
-    myFile.open(fileName.c_str());
-    myFile >> dimension1 >> dimension2;
-    erase(g,dimension1,dimension2);
-    for(int x =0;x <dimension1;x++)
-    {
-        for(int y=0; y<dimension2; y++)
-        {
-            myFile >> R >> G >> B;
-            g.plotPixel(loc.x+x,loc.y+y,R,G,B);
-        }
-    }
-    g.Sleep(100);
+
 }
 
-void Pokemon:: erase(SDL_Plotter& g,int dimension1,int dimension2)
+void Pokemon:: createSprite(string pokemonName)
 {
-    for(int x= 0;x<dimension1;x++)
-    {
-        for(int y =0;y<dimension2;y++)
-        {
-            g.plotPixel(oldLoc.x +x,oldLoc.y+y,255,255,255);
+    ifstream pokemonFile;
+    pokemonFile.open(pokemonName.c_str());
+    pokemonFile >> dimension1 >> dimension2;
+    for(int r = 0; r < dimension1; r++){
+        for(int c = 0; c < dimension2; c++){
+            pokemonFile >> pic[r][c].R;
+            pokemonFile >> pic[r][c].G;
+            pokemonFile >> pic[r][c].B;
         }
     }
+    pokemonFile.close();
 }
+
+
+void Pokemon:: draw(SDL_Plotter& g)
+{
+    erase(g);
+    for(int y =0;y <dimension1;y++){
+        for(int x=0; x<dimension2; x++){
+
+            if(pic[y][x].R == 255 && pic[y][x].G == 255 && pic[y][x].B == 255)
+            {
+                pic[y][x].R = background[loc.x+x][loc.y+y].R;
+                pic[y][x].G = background[loc.x+x][loc.y+y].G;
+                pic[y][x].B = background[loc.x+x][loc.y+y].B;
+            }
+
+            g.plotPixel(loc.x+x,loc.y+y,pic[y][x].R, pic[y][x].G, pic[y][x].B);
+        }
+    }
+
+}
+
+ void Pokemon:: erase(SDL_Plotter& g){
+     for(int y= 0;y<dimension1;y++){
+        for(int x =0;x<dimension2;x++){
+                g.plotPixel(oldLoc.x +x,oldLoc.y+y,background[oldLoc.x + x][oldLoc.y + y].R,background[oldLoc.x + x][oldLoc.y + y].G,background[oldLoc.x + x][oldLoc.y + y].B);
+        }
+     }
+ }
+
 
 void Pokemon::move(DIR d)
 {
-    oldLoc = loc;
+    oldLoc.x = loc.x;
+    oldLoc.y = loc.y;
     switch(d)
     {
         case UP:    loc.y-=speed;
@@ -101,10 +120,30 @@ void Pokemon::move(DIR d)
                     break;
     }
 }
-
-void Pokemon::setLoc(Point p)
-{
-    loc = p;
+void Pokemon:: setLoc(Point p){
+    loc.y = p.y;
+    loc.x = p.x;
 }
+
+void Pokemon::scopeBackground(vector <vector<Point> > scopeBackground)
+{
+    scopeBackground.resize(1000, vector <Point>(1000));
+    background.resize(1000, vector <Point>(1000));
+    background = scopeBackground;
+}
+
+void setNamesFileName(string a[256])
+{
+    int counter = 0;
+    ifstream indexFile;
+    indexFile.open("pokemon.txt");
+    do
+    {
+        indexFile >> a[counter];
+        counter++;
+    }while(!indexFile.eof());
+    indexFile.close();
+}
+
 
 #endif // POKEMON_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,53 +1,246 @@
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
 #include <ctime>
-
+#include <cstdlib>
 #include "SDL_Plotter.h"
 #include "Pokemon.h"
+#include "background.h"
+#include "player.h"
+#include "pokeball.h"
+#include "message.h"
 
 using namespace std;
 
 int main(int argc, char ** argv)
 {
+    bool messageShow = false;
+    int dontStop = 0;
+    int frame = 0;
+    int direction;
     SDL_Plotter g(1000,1000);
-    Pokemon a,b;
-    char key,direction;
-    string pokemonName[] = {"image.txt"};
+    Pokemon a;
+    Pokemon b;
+    Background c;
+    Player d;
+    Pokeball e;
+    Message f;
+    string pokemen[256];
+    setNamesFileName(pokemen);
+    a.pokemonName = pokemen[0];
+    b.pokemonName = pokemen[1];
 
-    a.setLoc(Point(500,500));
-    b.setLoc(Point(250,250));
+    char Key;
+    char Random;
 
-    while(!g.getQuit())
+    c.setBackground();
+    c.drawBackground(g);
+    a.setLoc(c.objectBackground[200][300]);
+    a.scopeBackground(c.objectBackground);
+    a.createSprite(pokemen[0]);
+    b.setLoc(c.objectBackground[100][200]);
+    b.scopeBackground(c.objectBackground);
+    b.createSprite(pokemen[1]);
+    d.setLoc(c.objectBackground[500][500]);
+    d.scopePlayerBackground(c.objectBackground);
+    d.createSprite("TFD.txt");
+    e.scopeBallBackground(c.objectBackground);
+    f.setLoc(c.objectBackground[300][300]);
+    f.scopeMessageBackground(c.objectBackground);
+    f.createSprite("message.txt");
+
+
+    while (!g.getQuit())
     {
-        a.draw(g,pokemonName[0]);
-        b.draw(g,pokemonName[0]);
+        if(b.deleted == false)
+        {
+            b.draw(g);
+        }
+        if(a.deleted == false)
+        {
+            a.draw(g);
+        }
+        d.draw(g);
         g.update();
-    	if(true)
-    	{
-            srand(time(NULL));
-    	    key = rand()%4+1;
-    	    direction = rand()%4+1;
-    	    switch(key)
+
+        srand(time(NULL));
+        Random = rand()%4+1;
+        if(g.kbhit()){
+    	    Key =g.getKey();
+
+    	    switch(Key){
+
+            case UP_ARROW: direction = 0;
+                           d.createSprite("TFU.txt");
+                           d.move(UP);
+                           if(frame == 0)
+                           {
+                               g.Sleep(100);
+                               d.createSprite("TFU-R.txt");
+                               frame = 1;
+                           }
+                           else if(frame == 1)
+                           {
+                               g.Sleep(100);
+                               d.createSprite("TFU.txt");
+                               frame = 2;
+                           }
+                           else if(frame == 2)
+                           {
+                               g.Sleep(100);
+                               d.createSprite("TFU-L.txt");
+                               frame = 3;
+                           }
+                           else if(frame == 3)
+                           {
+                               g.Sleep(100);
+                               d.createSprite("TFU.txt");
+                               frame = 0;
+                           }
+                           break;
+            case DOWN_ARROW: d.createSprite("TFD.txt");
+                             d.move(DOWN);
+                             direction = 1;
+                             if(frame == 0)
+                             {
+                                 g.Sleep(100);
+                                 d.createSprite("TFD-R.txt");
+                                 frame = 1;
+                             }
+                             else if(frame == 1)
+                             {
+                                 g.Sleep(100);
+                                 d.createSprite("TFD.txt");
+                                 frame = 2;
+                             }
+                             else if(frame == 2)
+                             {
+                                 g.Sleep(100);
+                                 d.createSprite("TFD-L.txt");
+                                 frame = 3;
+                             }
+                             else if(frame == 3)
+                             {
+                                 g.Sleep(100);
+                                 d.createSprite("TFD.txt");
+                                 frame = 0;
+                             }
+                             break;
+            case LEFT_ARROW: d.createSprite("TFL.txt");
+                             d.move(LEFT);
+                             direction = 2;
+                             if(frame == 0)
+                             {
+                                 g.Sleep(100);
+                                 d.createSprite("TFL-R.txt");
+                                 frame = 1;
+                             }
+                             else if(frame == 1)
+                             {
+                                 g.Sleep(100);
+                                 d.createSprite("TFL.txt");
+                                 frame = 2;
+                             }
+                             else if(frame == 2)
+                             {
+                                 g.Sleep(100);
+                                 d.createSprite("TFL-L.txt");
+                                 frame = 3;
+                             }
+                             else if(frame == 3)
+                             {
+                                 g.Sleep(100);
+                                 d.createSprite("TFL.txt");
+                                 frame = 0;
+                             }
+                             break;
+            case RIGHT_ARROW: d.createSprite("TFR.txt");
+                              d.move(RIGHT);
+                              direction = 3;
+                              if(frame == 0)
+                              {
+                                  g.Sleep(100);
+                                  d.createSprite("TFR-R.txt");
+                                  frame = 1;
+                              }
+                              else if(frame == 1)
+                              {
+                                  g.Sleep(100);
+                                  d.createSprite("TFR.txt");
+                                  frame = 2;
+                              }
+                              else if(frame == 2)
+                              {
+                                  g.Sleep(100);
+                                  d.createSprite("TFR-L.txt");
+                                  frame = 3;
+                              }
+                              else if(frame == 3)
+                              {
+                                  g.Sleep(100);
+                                  d.createSprite("TFR.txt");
+                                  frame = 0;
+                              }
+                              break;
+            case SPACE: e.createSprite();
+                        e.setLoc(d.loc);
+                        dontStop++;
+                        break;
+    	    }
+
+        }
+
+        switch(Random)
+        {
+            case UP_ARROW:      a.move(UP);
+                                b.move(UP);
+                                break;
+            case DOWN_ARROW:    a.move(DOWN);
+                                b.move(DOWN);
+                                break;
+            case LEFT_ARROW:    a.move(LEFT);
+                                b.move(LEFT);
+                                break;
+            case RIGHT_ARROW:   a.move(RIGHT);
+                                b.move(RIGHT);
+                                break;
+        }
+
+        if(dontStop != 0)
+        {
+            while(dontStop != 0)
             {
-                case UP_ARROW:      a.move(UP);
-                                    break;
-                case DOWN_ARROW:    a.move(DOWN);
-                                    break;
-                case LEFT_ARROW:    a.move(LEFT);
-                                    break;
-                case RIGHT_ARROW:   a.move(RIGHT);
-                                    break;
+                e.move(direction);
+                e.draw(g);
+                e.createSprite();
+                d.draw(g);
+                g.update();
+
+                if((e.loc.x <= 100 || e.loc.y <= 100 || e.loc.x >= 900 || e.loc.y >= 900)
+                   || ballCollide(e, a) == true)
+                {
+                    f.setLoc(e.loc);
+                    dontStop = 0;
+                    messageShow = true;
+                }
             }
-            switch(direction)
-            {
-                case UP_ARROW:      b.move(UP);
-                                    break;
-                case DOWN_ARROW:    b.move(DOWN);
-                                    break;
-                case LEFT_ARROW:    b.move(LEFT);
-                                    break;
-                case RIGHT_ARROW:   b.move(RIGHT);
-                                    break;
-            }
+        }
+
+        if(messageShow == true)
+        {
+            f.scopeMessageBackground(c.objectBackground);
+            f.createSprite("message.txt");
+            f.draw(g);
+            e.erase(g);
+            a.deleted = true;
+            a.erase(g);
+            g.update();
+        }
+
+
+
+
     	}
-    }
+    	return 0;
 }
